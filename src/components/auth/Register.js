c54fc7c8b14c928652cf938ae5b690ebad970335
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-05-25 12:04:38 +0200

    axios added and test user created

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,38 +1,106 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import axios from "axios";
 
-const Register = () => (
-  <Fragment>
-    <h1 className="large text-primary">Sign Up</h1>
-    <p className="lead">
-      <i className="fas fa-user"></i> Create Your Account
-    </p>
-    <form className="form" action="create-profile.html">
-      <div className="form-group">
-        <input type="text" name="name" required />
-        <label>Name</label>
-      </div>
-      <div className="form-group">
-        <input type="email" name="email" />
-        <label>Email</label>
-        <small className="form-text">
-          This site uses Gravatar so if you want a profile image, use a Gravatar
-          email
-        </small>
-      </div>
-      <div className="form-group">
-        <input type="password" name="password" minLength="6" />
-        <label>Password</label>
-      </div>
-      <div className="form-group">
-        <input type="password" name="password2" minLength="6" />
-        <label>Confirm password</label>
-      </div>
-      <input type="submit" className="btn btn-primary" value="Register" />
-    </form>
-    <p className="my-1">
-      Already have an account? <a href="login.html">Sign In</a>
-    </p>
-  </Fragment>
-);
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords do not match ");
+    } else {
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        const body = JSON.stringify(newUser);
+
+        const res = await axios.post("/api/users", body, config);
+        console.log(res.data);
+      } catch (err) {
+        console.error(err.response.data);
+      }
+    }
+  };
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+          <label style={name ? { display: "none" } : {}}>Name</label>
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
+          <label style={email ? { display: "none" } : {}}>Email</label>
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+          <label style={password ? { display: "none" } : {}}>Password</label>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password2"
+            minLength="6"
+            value={password2}
+            onChange={(e) => onChange(e)}
+          />
+          <label style={password2 ? { display: "none" } : {}}>
+            Confirm password
+          </label>
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+    </Fragment>
+  );
+};
 
 export default Register;
